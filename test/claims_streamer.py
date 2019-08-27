4dea925144aa5feda1deea3e394ea69dd9bc2247
PK <pradhyu.shrestha@gmail.com>
2019-08-26 22:04:05 -0400

    initial work on streamer

--- test/claims_streamer.py
+++ test/claims_streamer.py
@@ -5,18 +5,24 @@ import logging
 import pika
 import json
 
+from pika import PlainCredentials, ConnectionParameters
+from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
+
 logging.basicConfig(level=logging.ERROR)
 
-credentials = pika.PlainCredentials('guest', 'guest')
-parameters = pika.ConnectionParameters('localhost', credentials=credentials, port=5672)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+
+credentials: PlainCredentials = pika.PlainCredentials('guest', 'guest')
+parameters: ConnectionParameters = pika.ConnectionParameters('localhost', credentials=credentials, port=5672)
+connection: BlockingConnection = pika.BlockingConnection(parameters)
+channel: BlockingChannel = connection.channel()
 channel.exchange_declare(
     exchange="test_exchange",
     exchange_type="topic",
     passive=False,
     durable=True,
     auto_delete=False)
+
+# open the test file as payload
 with open('../tests/test_01.json') as f:
     payload = json.load(f)
     f.close()
@@ -25,7 +31,7 @@ print("Sending json message to a queue")
 channel.basic_publish(
     exchange='test_exchange',
     routing_key='standard_key',
-    body=json.dumps(payload),
+    body=json.dumps(payload), # need to dump the payload as body
     properties=pika.BasicProperties(content_type='application/json', delivery_mode=1))
 
 connection.sleep(5)
