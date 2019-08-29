cbbe44ebe5fa8746e0dc672efece8f34d7f3fafb
PK <pradhyu.shrestha@gmail.com>
2019-08-29 00:46:48 -0400

    initial work on streamer

--- test/claims_streamer.py
+++ test/claims_streamer.py
@@ -29,8 +29,12 @@ with open('../tests/test_01.json') as f:
 num_of_trials = 5
 print("Sending json message to a queue")
 for i in range(num_of_trials):
+    payload["id"]= i
+    payload["object"]['object']['object1']= i
+    payload["object"]['object']['object']= f"objectoos{i}"
+
     channel.basic_publish(
-        exchange='test_exchange',
+        exchange='amq.topic',
         routing_key='standard_key',
         body=json.dumps(payload),  # need to dump the payload as body
         properties=pika.BasicProperties(content_type='application/json', delivery_mode=1))
@@ -50,19 +54,19 @@ for method_frame, properties, body in channel.consume('khai'):
     if method_frame.delivery_tag == num_of_trials:
         break
 # Cancel the consumer and return any pending messages
-requeued_messages: int = channel.cancel()
-print('Requeued %i messages=' % requeued_messages)
+#requeued_messages: int = channel.cancel()
+#print('Requeued %i messages=' % requeued_messages)
 
 print("Sending text message to group")
 channel.basic_publish(
-    'test_exchange', 'group_key', 'Message to group_key',
+    'amq.topic', 'group_key', 'Message to group_key',
     pika.BasicProperties(content_type='text/plain', delivery_mode=1))
 
 connection.sleep(5)
 
 print("Sending text message")
 channel.basic_publish(
-    'test_exchange', 'standard_key', 'Message to standard_key',
+    'amq.topic', 'standard_key', 'Message to standard_key',
     pika.BasicProperties(content_type='text/plain', delivery_mode=1))
 
 
